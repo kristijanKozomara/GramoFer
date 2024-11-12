80eb415e94fcf8c3c10c56d31b2bb45d0a0c8e3e
kristijanKozomara <kristijan.kozomara@fer.unizg.hr>
2024-11-12 08:41:12 +0100

    Update main.tsx
    main prilagođen za google sign in

--- Frontend/GramoFER/src/main.tsx
+++ Frontend/GramoFER/src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="53411913471-urmj121lk57fvu9c91u832podj4ipbk3.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>  
   </StrictMode>
 );
+
