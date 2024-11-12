336ce7854ba06a5792ecb416ef27b72e1ceee70b
kristijanKozomara <kristijan.kozomara@fer.unizg.hr>
2024-11-12 08:37:38 +0100

    Update Header.tsx
    updatejt headera, dodan google sign in

--- Frontend/GramoFER/src/compotents/Header/Header.tsx
+++ Frontend/GramoFER/src/compotents/Header/Header.tsx
@@ -1,16 +1,24 @@
 import styles from "./Header.module.css";
+import { GoogleLogin } from '@react-oauth/google';
+
 function Header() {
+
+  const handleLoginSuccess = (response: any) => {
+    console.log("Login Success: ", response);
+    // Handle the response, such as storing the token in local storage or state
+  };
+
+  const handleLoginFailure = () => {
+    console.log("Login Failed");
+  };
+
   return (
     <div className={styles.header}>
       <div className={[styles.main_column, styles.row].join(" ")}>
         <h1>GramoFER</h1>
       </div>
       <div></div>
-      <div
-        className={[styles.main_column, styles.row, styles.site_links].join(
-          " "
-        )}
-      >
+      <div className={[styles.main_column, styles.row, styles.site_links].join(" ")}>
         <div>
           <h2>Ponude</h2>
         </div>
@@ -20,8 +28,17 @@ function Header() {
         <div>
           <h2>Moje zamjene</h2>
         </div>
+        <div>
+          <GoogleLogin
+            onSuccess={handleLoginSuccess}
+            onError={handleLoginFailure}
+            useOneTap
+            
+          />
+        </div>
       </div>
     </div>
   );
 }
+
 export default Header;
